99c77e513a3ac223f0407917c372e7237a5274bf
janParamo <janparamo@uamv.edu.ni>
2024-04-04 18:25:36 -0600

    agrugue mi nombre

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    printf("Sesion de reforzamiento #2")
+    printf("Sesion de reforzamiento #2");
+    printf("Jan Carlo");
     return 0;
 }
